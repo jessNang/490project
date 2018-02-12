775e82a8ae3cac61e3ed35e7d6cbc4900ba9aa86
roccotg8 <rtg8@njit.edu>
2018-02-12 17:32:14 -0500

    finished the discover request to the api. it properly returns the results given the search terms

--- APIRequests/movieAPIDiscover.php
+++ APIRequests/movieAPIDiscover.php
@@ -2,7 +2,7 @@
 $param = array(false,false,false,false,false,false,false,false,false,false);
 $paramPos = array(array(),array(),array(),array(),array(),array(),array(),array(),array(),array());
 $paramValue = array(array(),array(),array(),array(),array(),array(),array(),array(),array(),array());
-$paramName = array("noKeyword","noGenre","year","people","keyword","genre","company","crew","cast","adult");
+$paramName = array("without_keywords","without_genres","year","with_people","with_keyword","with_genres","with_companys","with_crew","with_cast","include_adult");
 $final = array();
 $finalName = array();
 
@@ -32,13 +32,13 @@ for($i = 0; $i < count($param); $i++)
 {
 	if($param[$i])
 	{
-		echo "For " . $paramName[$i] . "Adding:".PHP_EOL;
+		//echo "For " . $paramName[$i] . "Adding:".PHP_EOL;
 		for($j = 0; $j < count($paramPos[$i]);$j++)
 		{
 			$offset = 1;
 			while(($paramPos[$i][$j] + $offset) < $argc && substr($argv[$paramPos[$i][$j] + $offset],0,1) != "-")
 			{
-				echo $argv[$paramPos[$i][$j] + $offset].PHP_EOL;
+				//echo $argv[$paramPos[$i][$j] + $offset].PHP_EOL;
 				array_push($paramValue[$i], $argv[$paramPos[$i][$j] + $offset]);
 				$offset++;
 			}
@@ -59,22 +59,28 @@ for($i = 0; $i < count($param); $i++)
 	{
 		array_push($final,array());
 		$final[$finalcount] = $paramValue[$i];//$final is an array of the search terms
-		array_push($finalName,$paramName[$i]);//$finalName is an array of 
+		array_push($finalName,$paramName[$i]);//$finalName is an array of the search fields that are used
 		$finalcount++;
 	}
 }
 
-print_r($finalName);
-
-
-
-/*
-$SearchParameters = "without_keywords={$parameters[0]}&without_genres={$parameters[1]}&year={$parameters[2]}&with_people={$parameters[3]}&with_keywords={$parameters[4]}&with_genres={$parameters[5]}&with_companies={$parameters[6]}&with_crew={$parameters[7]}&with_cast={$$parameters[8]}&include_adult={$$parameters[9][0]}"
+$searchParameters = '';
+for($i = 0; $i < count($finalName); $i++)
+{
+	$searchParameters .= "$finalName[$i]=";
+	for($j = 0; $j < count($final[$i]); $j++)
+	{
+		if($j == 0)
+			$searchParameters .= $final[$i][$j];
+		else
+			$searchParameters .= "%20" . $final[$i][$j];
+	}
+}
 
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.themoviedb.org/3/discover/movie?$SearchParameters&sort_by=popularity.desc&language=en-US&api_key=78d3b2e412d269add2b072f074d49fa3&page=1&include_video=false",
+  CURLOPT_URL => "https://api.themoviedb.org/3/discover/movie?api_key=78d3b2e412d269add2b072f074d49fa3&sort_by=popularity.desc&language=en-US&$searchParameters&page=1&include_video=false",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
@@ -94,5 +100,5 @@ if ($err) {
 } else {
   echo $response;
 }
-*/
+
 ?>
