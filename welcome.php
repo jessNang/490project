a8ce44e9ae75eab10d9265543da75022db79a167
jessNang <31712591+jessNang@users.noreply.github.com>
2018-02-12 17:18:56 -0500

    simple welcome page working

--- welcome.php
+++ welcome.php
@@ -1,4 +1,5 @@
 <?php
+#starts session and connects to the one from the login page
 session_start();
 if(!isset($_SESSION["sess_user"])){
 	header("location:login.php");
@@ -7,10 +8,28 @@ if(!isset($_SESSION["sess_user"])){
 <!doctype html>
 <html>
 <head>
-<title>Welcome</title>
+	<meta charset="utf-8">
+	<title>Welcome</title>
+	<link rel="stylesheet" href="welcome.css">
 </head>
 <body>
-<h2>Welcome to JLEOMD, <?=$_SESSION['sess_user'];?>! <a href="logout.php">Click here to logout</a></h2>
+	<nav>
+		<ul class="main_menu">
+			<li><a href="welcome.php">Home</a></li>
+			<li><a href="#">Action</a></li>
+			<li><a href="#">Adventure</a></li>
+			<li><a href="#">Comedy</a></li>
+			<li><a href="#">Family</a></li>
+			<li><a href="#">Horror</a></li>
+			<li><form>
+				<input type="search" placeholder"search...">
+				<a href="#" class="fa fa-search"></a>
+			</form></li>
+			<li><a href="logout.php">Logout</a></li>
+		</ul>
+	</nav>
+	</div>
+	<h2>Welcome to JLEOMD, <?=$_SESSION['sess_user'];?>!</h2>
 </body>
 </html>
 <?php
