9c8f98abdc73a15a0c92dc1ece0f82e58e10c65d
jessNang <31712591+jessNang@users.noreply.github.com>
2018-02-15 17:13:27 -0500

    welcome page working w/ session timeout (30 mins)

--- welcome.php
+++ welcome.php
@@ -1,5 +1,5 @@
 <?php
-#starts session and connects to the one from the login page
+#starts session and connects to the user
 session_start();
 if(!isset($_SESSION["sess_user"])){
 	header("location:login.php");
@@ -33,5 +33,31 @@ if(!isset($_SESSION["sess_user"])){
 </body>
 </html>
 <?php
+
+
+//Expire the session if user is inactive for 30 minutes or more.
+$expireAfter = 30;
+ 
+//Check to see if our "last action" session variable has been set.
+if(isset($_SESSION['last_action'])){
+    
+    //Figure out how many seconds have passed since the user was last active.
+    $secondsInactive = time() - $_SESSION['last_action'];
+    
+    //Convert our minutes into seconds.
+    $expireAfterSeconds = $expireAfter * 60;
+    
+    //Check to see if they have been inactive for too long.
+    if($secondsInactive >= $expireAfterSeconds){
+        //User has been inactive for too long. Kill their session.
+        session_unset();
+        session_destroy();
+	header("location:login.php");
+    }
+    
+}
+ 
+//Assign the current timestamp as the user's latest activity
+$_SESSION['last_action'] = time();
 }
 ?>
