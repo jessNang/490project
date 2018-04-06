29862507dba97dd7045f30b9a07f740d88c7227b
jessNang <31712591+jessNang@users.noreply.github.com>
2018-04-06 13:50:43 -0400

    pages for upcoming

--- upcoming.php
+++ upcoming.php
@@ -1,21 +1,23 @@
 <?php
+#upcoming page 1
 #starts session and connects to the user
 session_start();
 if(!isset($_SESSION["sess_user"])){
         header("location:login.php");
 } else {
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
         <meta charset="utf-8">
-        <title>Upcoming</title>
-	<link rel="stylesheet" href="rate.css">
+        <title>Upcoming Page 1</title>
+		<link rel="stylesheet" href="rate.css">
         <link rel="stylesheet" href="welcome.css">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
 </head>
 <body>
+		<!-- Navigation bar -->
         <nav>
                 <ul class="main_menu">
                         <li><a href="welcome.php">Home</a></li>
@@ -38,19 +40,24 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+//if user typed movie title into search bar
 if((isset($_REQUEST['search']))&&($_REQUEST['search']!="")){
 	$title=$_REQUEST['search'];
 	$category = array();
 	array_push($category, $title);
 	$client = new rabbitMQClient("dmz.ini","testServer");
 
+	//api request array for movie user searched
 	$request = array();
 	$request['type'] = "find";
 	$request['params'] = $category;
 	$request['page'] = "";
 	$response = $client->send_request($request);
 	$movieTitle;
+	
+	//Printing api results
 	if($response == true){
+			//Movie poster
         	foreach($movie as $key => $value){
                 	if($key=="poster_path"){
 #                       	echo "<table style='width:100%'><td><img src='https://image.tmdb.org/t/p/w342".$value."'></td>"; 
@@ -59,6 +66,8 @@ if((isset($_REQUEST['search']))&&($_REQUEST['search']!="")){
                         	#echo "<br>";
                 	}
         	}
+			
+			//Movie title
         	foreach($response['data'] as $key => $value){
                 	if($key=="title"){
                         	#echo "<td>$value<br><br>";
@@ -66,9 +75,12 @@ if((isset($_REQUEST['search']))&&($_REQUEST['search']!="")){
                         	$movieTitle=$value;
                 	}
         	}
+			
+			//Movie Rating
         	echo "Rating: ";
         ?>
         <form>
+		<!--Rating stars -->
         <fieldset class="starability-growRotate">
                 <input type="radio" id="rate5" name="rating" value="5" />
                 <label for="rate5" title="Terrible">5 stars</label>
@@ -87,11 +99,14 @@ if((isset($_REQUEST['search']))&&($_REQUEST['search']!="")){
         </fieldset>
         </form>
 	<?php
-        	foreach($response['data'] as $key => $value){
+        	//Movie's release date
+			foreach($response['data'] as $key => $value){
                 	if($key=="release_date"){
                         	echo "Release Date: $value<br><br>";
                 	}
         	}
+			
+			//Genres associated with the movie
         	foreach($response['data'] as $key => $value){
                 	if($key=="genre_ids"){
                         	echo "Genre: ";
@@ -101,6 +116,8 @@ if((isset($_REQUEST['search']))&&($_REQUEST['search']!="")){
                         	echo "<br><br>";
                 	}
         	}
+			
+			//Movie's overview
         	foreach($response['data'] as $key => $value){
                 	if($key=="overview"){
                         	#echo "Overview: $value<br></td></tr></table><br>";
@@ -108,32 +125,41 @@ if((isset($_REQUEST['search']))&&($_REQUEST['search']!="")){
                 	}
         	}
 	}
+	
+	////Link to find similar movies to current movie
 	echo "<a href='movieRecommend.php?movie=".$movieTitle."'>Similar Movies</a><br>";
 }
+
 
 else{
 	$client = new rabbitMQClient("dmz.ini","testServer");
 
+	//upcoming movies page 1 api request
 	$request = array();
 	$request['type'] = "upcoming";
 	$request['page'] = "";
 	$response = $client->send_request($request);
 
+	//printing out the api results
 	if($response == true){
 		foreach($response['data'] as $movie){
 			echo "<br>";
+			
+			//Movie Poster
 			foreach($movie as $key => $value){
                         	if($key=="poster_path"){
                                 	echo "<img src='https://image.tmdb.org/t/p/w300".$value."' height='150'>";
                                 	echo "<br>";
                         	}
 			}
-
+			
+			//Movie title
 			foreach($movie as $key => $value){
                         	if($key=="title"){
+									//link to page with detailed info about the movie
                                		echo "<a href='movieFind.php?category=".$value."'>$value</a><br>";
                         	}
-
+				//Movie's release date
 				if($key=="release_date"){
 					echo "Release Date: $value<br>";
 				}
@@ -142,6 +168,13 @@ else{
 	}
 }
 ?>
+	<footer>
+		<p>
+		<a href="upcoming.php">1 &nbsp;</a><br>";
+		<a href="upcoming2.php">2 &nbsp;</a><br>";
+		<a href="upcoming3.php">3 &nbsp;</a><br>";
+		</p>
+	</footer>
 </body>
 </html>
 <?php
