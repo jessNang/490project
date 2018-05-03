a9790f4eb3d89c04e47508e4041cf6b7520be31a
jessNang <31712591+jessNang@users.noreply.github.com>
2018-05-03 17:32:25 -0400

    still testing

--- showtimes.php
+++ showtimes.php
@@ -1,6 +1,10 @@
 <?php
-// Start the session
+#Showtimes
+#starts session and connects to the user
 session_start();
+if(!isset($_SESSION["sess_user"])){
+        header("location:login.php");
+} else {
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,6 +33,8 @@ session_start();
             <li><a href="upcoming.php">Upcoming</a></li>
             <li><a href="classics.php">Classics</a></li>
             <li><a href="discover.php">Discover</a></li>
+			<li><a href="showtimes.php">Showtimes</a></li>
+			<li><a href="forum.php">Forum</a></li>
             <li><form method="post">
                 <input type="search" name="search" placeholder="Search movies...">
                 <a class="fa fa-search"></a>
@@ -96,3 +102,31 @@ if(isset($_POST["submit"])){
 ?>
 </body>
 </html>
+<?php
+//Expire the session if user is inactive for 30 minutes or more.
+$expireAfter = 30;
+
+//Check to see if our "last action" session variable has been set.
+if(isset($_SESSION['last_action'])){
+
+    //Figure out how many seconds have passed since the user was last active.
+    $secondsInactive = time() - $_SESSION['last_action'];
+
+    //Convert our minutes into seconds.
+    $expireAfterSeconds = $expireAfter * 60;
+
+    //Check to see if they have been inactive for too long.
+    if($secondsInactive >= $expireAfterSeconds){
+        //User has been inactive for too long. Kill their session.
+        session_unset();
+        session_destroy();
+        header("location:login.php");
+    }
+
+}
+
+//Assign the current timestamp as the user's latest activity
+$_SESSION['last_action'] = time();
+}
+?>
+
